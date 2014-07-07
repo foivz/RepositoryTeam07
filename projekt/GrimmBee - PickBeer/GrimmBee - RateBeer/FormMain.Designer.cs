a7c0e52ad7dc8b6f83a727983a187bd3ba838245
gloga <glogabmf@gmail.com>
2014-07-07 18:56:22 +0200

    PickBeer_Admin
    Zavrsena aplikacija za administratora (dodavanje, pregled i izmjenu
    artikala)

---
+++ projekt/GrimmBee - PickBeer/GrimmBee - RateBeer/FormMain.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace GrimmBee___RateBeer
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonView = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAdd.Location = new System.Drawing.Point(82, 12);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.Text = "Add New";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonView
+            // 
+            this.buttonView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonView.Location = new System.Drawing.Point(82, 41);
+            this.buttonView.Name = "buttonView";
+            this.buttonView.Size = new System.Drawing.Size(75, 23);
+            this.buttonView.TabIndex = 1;
+            this.buttonView.Text = "View All";
+            this.buttonView.UseVisualStyleBackColor = true;
+            this.buttonView.Click += new System.EventHandler(this.buttonView_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUpdate.Location = new System.Drawing.Point(82, 70);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 2;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 102);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonView);
+            this.Controls.Add(this.buttonAdd);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "FormMain";
+            this.Text = "Main Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonView;
+        private System.Windows.Forms.Button buttonUpdate;
+    }
+}
+
---
+++ projekt/GrimmBee - PickBeer/GrimmBee - RateBeer/FormUpdate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrimmBee___RateBeer
+{
+    public partial class FormUpdate : Form
+    {
+        public FormUpdate()
+        {
+            InitializeComponent();
+        }
+
+        private void pivoBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.pivoBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.t07_DBDataSet11);
+
+        }
+
+        private void FormUpdate_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 't07_DBDataSet11.Pivo' table. You can move, or remove it, as needed.
+            this.pivoTableAdapter.Fill(this.t07_DBDataSet11.Pivo);
+            pictureBoxUpdate.ImageLocation = pivoDataGridViewUpdate.CurrentRow.Cells[10].Value.ToString();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.pivoBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.t07_DBDataSet11);
+            MessageBox.Show("Enty updated");
+        }
+    }
+}
