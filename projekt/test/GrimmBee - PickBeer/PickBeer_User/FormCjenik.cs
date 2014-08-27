480afc8d16dfe653eb5152e93eb5ff44440ccd21
ipokec <ivanpokec@yahoo.com>
2014-08-27 15:19:27 +0200

    Izrada testne verzije (čitajte)
    Izrađena je forma samo za ulaz gdje se pritisne gumb i ulazi se unutra.
    otvara se forma i može se dodavat u košarici ali još nije riješeno
    izmjena stanja košarice. moj posao još nije završe ali bi uskoro trebao
    bit gotov.

---
+++ projekt/test/GrimmBee - PickBeer/PickBeer_User/FormCjenik.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PickBeer_User
+{
+    public partial class FormCjenik : Form
+    {
+        public FormCjenik()
+        {
+            InitializeComponent();
+        }
+
+        private void pivoBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.pivoBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.t07_DBDataSet);
+
+        }
+
+        private void FormCjenik_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 't07_DBDataSet.Stavke_kosarica' table. You can move, or remove it, as needed.
+            this.stavke_kosaricaTableAdapter.Fill(this.t07_DBDataSet.Stavke_kosarica);
+            // TODO: This line of code loads data into the 't07_DBDataSet.Drzava_Select' table. You can move, or remove it, as needed.
+            this.drzava_SelectTableAdapter.Fill(this.t07_DBDataSet.Drzava_Select);
+            // TODO: This line of code loads data into the 't07_DBDataSet.Pivo' table. You can move, or remove it, as needed.
+            this.pivoTableAdapter.Fill(this.t07_DBDataSet.Pivo);
+            pictureBoxCjenik.ImageLocation = pivoDataGridViewCjenik.CurrentRow.Cells[12].Value.ToString();
+                   }
+
+        private void drzava_podrijetlaComboBox_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            String Drz = drzava_podrijetlaComboBox.SelectedValue.ToString();
+            this.pivoTableAdapter.FillByDrzava(t07_DBDataSet.Pivo, Drz);
+        }
+
+        private void pivoDataGridViewCjenik_SelectionChanged(object sender, EventArgs e)
+        {
+            if (pivoDataGridViewCjenik.RowCount > 0)
+            {
+                pictureBoxCjenik.ImageLocation = pivoDataGridViewCjenik.CurrentRow.Cells[12].Value.ToString();
+            }
+        }
+
+        private void buttonSve_Click(object sender, EventArgs e)
+        {
+            this.pivoTableAdapter.Fill(this.t07_DBDataSet.Pivo);
+        }
+
+        private void buttonDodajuK_Click(object sender, EventArgs e)
+        {
+            int Cjenik_ID = int.Parse(pivoDataGridViewCjenik.CurrentRow.Cells[0].Value.ToString());
+            Artikli a = new Artikli(Cjenik_ID);
+            a.Kolicina = int.Parse(kolicinaTextBox.Text.ToString());
+
+            
+
+           T07_DBDataSetTableAdapters.Stavke_kosaricaTableAdapter dodavanjeNovogArtikla = new T07_DBDataSetTableAdapters.Stavke_kosaricaTableAdapter();
+           dodavanjeNovogArtikla.Insert(Cjenik_ID, BrojStola.brojStola, a.Kolicina);
+
+            
+           
+
+
+
+
+            
+
+
+            
+            MessageBox.Show("Artikl dodan u košaricu");
+        }
+        
+    }
+}
---
+++ projekt/test/GrimmBee - PickBeer/PickBeer_User/FormGlavni.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PickBeer_User
+{
+    public partial class Glavni_izbornik : Form
+    {
+        public Glavni_izbornik()
+        {
+            InitializeComponent();
+            FormStol frmStol = new FormStol();
+            frmStol.ShowDialog();
+            
+            
+            
+            
+        }
+
+        private void buttonCjenik_Click(object sender, EventArgs e)
+        {
+            FormCjenik frmCjenik = new FormCjenik();
+            frmCjenik.ShowDialog();
+        }
+
+        
+        private void buttonKosarica_Click(object sender, EventArgs e)
+        {
+            FormKosarica frmKos = new FormKosarica();
+            frmKos.ShowDialog();
+        }
+
+        private void Glavni_izbornik_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
